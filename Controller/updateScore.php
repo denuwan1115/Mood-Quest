01fbf5f4c68e780a7a5f16a35face059df4baea8
denuwan1115 <denuwan2001abeysinghe.com>
2025-03-13 21:16:24 +0530

    create update score

---
+++ Controller/updateScore.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('memory_limit', '256M'); 
+
+include "../Controller/config.php";
+$data = json_decode(file_get_contents('php://input'), true); //get the json data
+//get the score and playerID from the json data
+$score = $data['score'];
+$playerID = $_SESSION['user_email'];
+$sql = "INSERT INTO `scores`(`playerID`, `score`) VALUES ('$playerID','$score')"; 
+if ($conn->query($sql) === TRUE) {
+    $response = array("status" => "success", "message" => "Game reset successfully");
+} else {
+    $response = array("status" => "error", "message" => "Error resetting game: " . $conn->error);
+}
+    $conn->close();
+header('Content-Type: application/json');
+echo json_encode($response);
+?>
